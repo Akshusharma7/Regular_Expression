e2137a929e5aa177c6d956b221b510d1dea7f42d
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-15 18:48:48 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -14,6 +14,10 @@ subbed_urls = pattern.sub(r'\2\3', urls)
 print(subbed_urls)
 
 # matches = pattern.finditer(urls)
-
+'''
+Group 0 - full urls
+Group 1 - www urls part
+Group 2 - doman name like google etc
+Group 3 - like .com, .net, .gov etc
 # for match in matches:
 #     print(match.group(3))
