1eb975d9789d473e3e663fadf32313e5d7053419
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-29 10:47:42 +0530

    Update email.py

--- email.py
+++ email.py
@@ -4,6 +4,7 @@ emails = '''
 CoreyMSchafer@gmail.com
 corey.schafer@university.edu
 corey-321-schafer@my-work.net
+prateekakshu
 '''
 
 pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
